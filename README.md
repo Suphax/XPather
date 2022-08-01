7db6758caf9b1ef4e0af85c58f13e0486295ce2a
Jack <19539889+Suphax@users.noreply.github.com>
2022-08-01 01:47:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# XPather
+
+Couldn't find a good XPath test app so threw together this one in an hour or two.
+
+Input XPath query, input XML, hit run and see the results.
+
+*Need to polish it up, as said it was done in 1-2 hours*
+
+See [https://www.w3schools.com/xml/xpath_syntax.asp](https://www.w3schools.com/xml/xpath_syntax.asp)
---
+++ XPather/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace XPather
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClearXML_Click(object sender, RoutedEventArgs e)
+        {
+            txtxml.Clear();
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.PreserveWhitespace = true;
+
+            string xPath = txtXPath.Text;
+
+            if (string.IsNullOrWhiteSpace(xPath))
+            {
+                lblStatus.Text = "XPath cannot be blank";
+                brdrStatus.ToolTip = "XPath cannot be blank";
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtxml.Text))
+            {
+                lblStatus.Text = "XML cannot be blank";
+                brdrStatus.ToolTip = "XML cannot be blank";
+                return;
+            }
+            lblStatus.Text = "";
+            brdrStatus.ToolTip = "";
+            txtoutput.Text = "";
+
+            try
+            {
+                xmlDocument.LoadXml(txtxml.Text);
+            }
+            catch (Exception ex)
+            {
+                string error = $"XML parsing error: {ex.Message}";
+                lblStatus.Text = error;
+                brdrStatus.ToolTip = error;
+                return;
+            }
+            XmlNodeList outputNodes;
+
+            try
+            {
+                outputNodes = xmlDocument.SelectNodes(xPath);
+            }
+            catch (Exception ex)
+            {
+                string error = $"XPath parsing error: {ex.Message}";
+                lblStatus.Text = error;
+                brdrStatus.ToolTip = error;
+                return;
+            }
+
+            if(outputNodes.Count == 0)
+            {
+                txtoutput.Text = "No matching XML!";
+                return;
+            }
+            // //strings[descendant::string[@name='OptimusTitle']]
+            var xmlOut = new StringBuilder();
+
+            foreach(XmlNode outputNode in outputNodes)
+            {
+                if(!string.IsNullOrWhiteSpace(outputNode.OuterXml))
+                    xmlOut.Append(outputNode.OuterXml + Environment.NewLine);
+                else
+                    xmlOut.Append(outputNode.InnerXml + Environment.NewLine);
+            }
+
+            txtoutput.Text = xmlOut.ToString();
+        }
+    }
+}
